f1fa2c1fcb5d8be6b57a96f910d684173d1f2352
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-04-26 06:30:42 -0400

    More vieport things :c

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,9 +15,9 @@ const Layout: FunctionComponent<LayoutProps> = ({
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Navbar />
       <div className={styles.bodyContainer}>{children}</div>
