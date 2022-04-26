ff767ba3ea0f1e07b78e114b5786dccfe646a504
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-04-26 06:30:42 -0400

    More vieport things :c

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout title="Manuel Rivas - Web Developer">
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout title="Manuel Rivas - Web Developer">
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 export default MyApp;
