ece7cece07b8f5f1bb9fd4d90de1b7d3d9f2ee15
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-04-26 06:18:43 -0400

    Sizing stuff

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,6 +17,7 @@ const Layout: FunctionComponent<LayoutProps> = ({
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Navbar />
       <div className={styles.bodyContainer}>{children}</div>
