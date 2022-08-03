ae0f0e0bee601980fb4519016868d1794b489064
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-08-03 09:09:29 -0400

    Adding new project ;)

--- mock/projects.ts
+++ mock/projects.ts
@@ -2,6 +2,7 @@ import { ProjectCardProps } from '../components/ProjectCard';
 import blogrImage from '../assets/images/frontend-mentor-blogr-landing-page.png';
 import calculatorAppImage from '../assets/images/multi-theme-calculator-app.png';
 import dolarVisualizerImage from '../assets/images/dolar-price-visualizer.png';
+import todoAppImage from '../assets/images/todo-app.png';
 
 const projects: ProjectCardProps[] = [
   {
@@ -19,6 +20,11 @@ const projects: ProjectCardProps[] = [
     badgeText: 'Dolar Price Visualizer',
     href: 'https://dolar-visualizer.herokuapp.com/',
   },
+  {
+    image: todoAppImage,
+    badgeText: 'FullStack Todo App',
+    href: 'https://mr-todo-app.herokuapp.com/'
+  }
 ];
 
 export default projects;
